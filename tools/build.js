c97d135e0b364afdecc81b8eed11b727c5e7739d
kassadube <noamsrb@yahoo.com>
2017-11-26 00:01:37 +0200

    add remove command

--- tools/build.js
+++ tools/build.js
@@ -5,7 +5,7 @@ import webpack from 'webpack';
 import config from '../webpack.config.prod';
 import {chalkError, chalkSuccess, chalkWarning, chalkProcessing} from './chalkConfig';
 
-process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
+process.env.NODE_ENV = 'production';//development'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
 
 console.log(chalkProcessing('Generating minified bundle. This will take a moment...'));
 
