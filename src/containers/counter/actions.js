a1676db7bfeb5e41412a58ea2a45e84c0278112c
kassadube <noamsrb@yahoo.com>
2018-10-29 17:29:13 +0200

    add actions

---
+++ src/containers/counter/actions.js
@@ -0,0 +1,16 @@
+export const increment = () => ({
+    type: 'INCREMENT',
+  })
+  
+  export const decrement = () => ({
+    type: 'DECREMENT',
+  })
+  
+  export const ping = () => ({
+    type: 'PING',
+  })
+  
+  export const pong = () => ({
+    type: 'PONG',
+  })
+  
