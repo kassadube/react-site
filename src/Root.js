0571d7ad9e4c866de783db22f262f51a51d66a17
kassadube <noamsrb@yahoo.com>
2018-10-30 09:58:10 +0200

    format root

--- src/Root.js
+++ src/Root.js
@@ -10,9 +10,9 @@ class Root  extends React.Component {
   render(){
       return(
         <div>
-        <Provider store={store}>
-          <App history={history} />
-        </Provider>
+          <Provider store={store}>
+            <App history={history} />
+          </Provider>
        </div>);
   }
 
